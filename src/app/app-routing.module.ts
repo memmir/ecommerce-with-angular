f5511be3f820d53c0e1baa00608bac37575738bc
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-24 16:42:38 +0300

    Created routing for layouts component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LayoutsComponent} from "./components/layouts/layouts.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
