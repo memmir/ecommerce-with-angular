649e6e2b7146b2136ce9288f739b7b337d1dd7ed
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-25 18:17:08 +0300

    Created Home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutsComponent } from './components/layouts/layouts.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutsComponent
+    LayoutsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
